8e349a56e48f3bfc96ec0da598fa0bb06c833ca6
Bieluu <lukasz.biel@winalife.pl>
2022-06-20 19:03:00 +0200

    Import fix, function tweaks

--- android/src/main/java/com/reactnativeopendefaultbrowser/OpenDefaultBrowserModule.java
+++ android/src/main/java/com/reactnativeopendefaultbrowser/OpenDefaultBrowserModule.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -30,9 +31,9 @@ public class OpenDefaultBrowserModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void openBrowser(Promise promise) {
         Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://"));
-        ResolveInfo resolveInfo = getPackageManager().resolveActivity(browserIntent,PackageManager.MATCH_DEFAULT_ONLY);
-        String packageName = resolveInfo.activityInfo.packageName;
         PackageManager packageManager = getReactApplicationContext().getPackageManager();
+        ResolveInfo resolveInfo = packageManager.resolveActivity(browserIntent,PackageManager.MATCH_DEFAULT_ONLY);
+        String packageName = resolveInfo.activityInfo.packageName;
         try {
             Intent launchIntent = packageManager.getLaunchIntentForPackage(packageName);
             getReactApplicationContext().startActivity(launchIntent);
