735f42ebac6036b68d394e835f3957571e44975b
Bieluu <lukasz.biel@winalife.pl>
2022-06-20 19:36:00 +0200

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,4 @@
-import { NativeModules, Platform } from 'react-native';
-
-const LINKING_ERROR =
-  `The package 'react-native-open-default-browser' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
+import { NativeModules } from 'react-native';
 
 const { OpenDefaultBrowser } = NativeModules;
 
